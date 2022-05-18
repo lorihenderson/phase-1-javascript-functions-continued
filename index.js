d478e0dac4928e3e751a0b74eba1ca61777696c7
Lori Henderson <lorim95@gmail.com>
2022-05-17 22:59:19 -0400

    commit tree cc367a4447c488ca755423ec6b657d9e53eb374d

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+
+function saturdayFun(hobby="roller-skate") {
+    return (`This Saturday, I want to ${hobby}!`);
+}
+saturdayFun();
+
+function mondayWork(work="go to the office") {
+    return (`This Monday, I will ${work}.`);
+}
+mondayWork();
+
+function wrapAdjective(emphasis="*") {
+    return function wrapAdjective(param="special") {
+        return (`You are ${emphasis}${param}${emphasis}!`)
+    }
+}
+wrapAdjective();
